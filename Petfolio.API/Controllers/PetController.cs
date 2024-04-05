b69bc6425d3919662b2b1846db011a490003bf1d
Flávio <f.augustosdn@gmail.com>
2024-04-04 22:52:39 -0300

    adicionado para retornar erros em lista

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -11,6 +11,7 @@ public class PetController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisteredPetJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestPetJson request)
     {
         var useCase = new RegisterPetUseCase();
@@ -21,6 +22,7 @@ public class PetController : ControllerBase
 
     [HttpPut("{id:int}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public IActionResult Update(int id, [FromBody] RequestPetJson request)
     {
         var useCase = new UpdatePetUseCase();
